cfaa3bf3f6e9dd0632a9242bca95e90cb4b58ffe
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2023-08-17 17:37:47 +0200

    add 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,3 +1,28 @@
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+// Реалізація делегування на ul.gallery і отримання url великого зображення.
+// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
+// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+
+// Посилання на оригінальне зображення повинно зберігатися в data-атрибуті source на елементі <img>, і вказуватися в href посилання. Не додавай інші HTML теги або CSS класи, крім тих, що містяться в цьому шаблоні.
+
+{
+  /* <li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</li>
+
+Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку. Заборони цю поведінку за замовчуванням. */
+}
+
+// Додай закриття модального вікна після натискання клавіші Escape. Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно. Бібліотека basicLightbox містить метод для програмного закриття модального вікна.
+
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
@@ -29,19 +54,24 @@ galleryListContainer.addEventListener("click", onGalleryListContainerClick);
 
 function onGalleryListContainerClick(evt) {
   evt.preventDefault();
+
   const largeImgUrl = evt.target.dataset.source;
   if (!largeImgUrl) {
     return;
   }
+
   const instance = basicLightbox.create(`
   <img
   class="gallery__image"
   src="${largeImgUrl}"
+  width="1280" 
+  height="auto"
 />
 `);
   instance.show();
 
   document.addEventListener("keydown", onEscKeyPress);
+
   function onEscKeyPress(evt) {
     const ESC = "Escape";
     if (evt.code === ESC) {
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryListContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(markupItems) {
+  return markupItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `;
+    })
+    .join("");
+}
+
+galleryListContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+var lightbox = new SimpleLightbox(".gallery a", {});
+
+new SimpleLightbox({ elements: ".imageGallery1 a" });
